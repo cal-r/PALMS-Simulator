76089dea87360a536515c6f2b8bdb97ca3863f08
Martin Fixman <martinfixman@gmail.com>
2025-11-17 14:41:10 +0000

    Fixing many issues when showing the scroll bar in AlphasBox, specially some race conditions.

--- PALMS.py
+++ PALMS.py
@@ -271,10 +271,6 @@ class PavlovianApp(QMainWindow):
             else:
                 self.per_cs_box[key].setVisible(True)
 
-        # Fill the scroll area in the alphas box with the content inside of it.
-        # Due to a bug (?), this does not happen automatically.
-        QTimer.singleShot(100, self.alphasBox.fillScrollArea)
-
     # Convenience function: convert a string to a float, or return None if empty.
     @classmethod
     def floatOrNone(cls, text: str) -> None | float:
