85f9438c67518ab7d06e87f4b6b4c8f1a0fa2e83
Martin Fixman <martinfixman@gmail.com>
2025-11-20 15:12:42 +0000

    Removing alpha_hall from alpha_mack calculation.

--- AdaptiveType.py
+++ AdaptiveType.py
@@ -268,15 +268,15 @@ class LePelleyHybrid(AdaptiveType):
         DVe = 0.
         DVi = 0.
         if rho >= 0:
-            DVe = s.alpha_mack * betap * s.alpha_hall * (1 - s.Ve + s.Vi) * abs(rho)
+            DVe = s.alpha_mack * s.alpha_hall * betap * (1 - s.Ve + s.Vi) * abs(rho)
 
             if rho > 0:
-                s.alpha_mack += -self.thetaE * s.alpha_hall * (abs(rp.lamda - s.Ve + s.Vi) - abs(rp.lamda - VXe + VXi))
+                s.alpha_mack += -self.thetaE * (abs(rp.lamda - s.Ve + s.Vi) - abs(rp.lamda - VXe + VXi))
         else:
-            DVi = s.alpha_mack * self.betan * s.alpha_hall * (1 - s.Vi + s.Ve) * abs(rho)
-            s.alpha_mack += -self.thetaI * s.alpha_hall * (abs(abs(rho) - s.Vi + s.Ve) - abs(abs(rho) - VXi + VXe))
+            DVi = s.alpha_mack * s.alpha_hall * self.betan * (1 - s.Vi + s.Ve) * abs(rho)
+            s.alpha_mack += -self.thetaI * (abs(abs(rho) - s.Vi + s.Ve) - abs(abs(rho) - VXi + VXe))
 
-        s.alpha_hall = self.gamma * abs(rho) + (1 - self.gamma) * s.alpha_hall
+        s.alpha_hall = self.gamma * (rp.lamda - rp.sigma) + (1 - self.gamma) * s.alpha_hall
         s.alpha_mack = min(max(s.alpha_mack, 0.05), 1)
         s.alpha_hall = min(max(s.alpha_hall, 0.5), 1)
 
