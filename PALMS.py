4bd1fa5f20f4fdb1cae8005148ba74db46813dc8
Martin Fixman <martinfixman@gmail.com>
2026-01-30 17:00:50 +0000

    Temporary commit with weird columns in table.

--- PALMS.py
+++ PALMS.py
@@ -574,9 +574,9 @@ class PavlovianApp(QMainWindow):
         self.plotBox.phaseBox.setCoordInfo(max(1 + event.xdata, 1), ylabel, event.ydata)
 
     def updateWidgets(self):
-        self.relax_size(self)
-        self.tableWidget.update()
-        self.tableWidget.repaint()
+        # self.relax_size(self)
+        # self.tableWidget.update()
+        # self.tableWidget.repaint()
         self.tableWidget.updateSizes()
         self.update()
         self.repaint()
